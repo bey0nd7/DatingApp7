682a2b491f68dbe86f967163ccec6e5d9e6c65ee
Mario Kainzbauer <mario.kainzbauer@gmail.com>
2024-01-20 10:30:19 +0100

    End of Section second commit test

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -30,5 +30,6 @@ public class UsersController : ControllerBase
     public async Task<ActionResult<AppUser>> GetUser(int id)
     {
         return await _context.Users.FindAsync(id);
+        // important comment for git
     }
 }
