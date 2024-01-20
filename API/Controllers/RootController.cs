e52e9af14c6689f847b8515b038c5a209d2f257d
Mario Kainzbauer <mario.kainzbauer@gmail.com>
2024-01-20 10:15:37 +0100

    End of section 2

---
+++ API/Controllers/RootController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+public class RootController : ControllerBase
+{
+    [HttpGet("Helloworld")]
+    public IActionResult Helloworld()
+    {
+        return Ok("Hello World");
+    }
+}
---
+++ API/Entities/AppFucker.cs
@@ -0,0 +1,7 @@
+﻿namespace API.Entities;
+
+public class AppFucker
+{
+    public int Id { get; set; }
+    public string? FuckerName {get; set;}
+}
