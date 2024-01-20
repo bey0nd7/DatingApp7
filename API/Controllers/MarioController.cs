74f327dc6a2e3b1040ee583361eb901577fde570
Mario Kainzbauer <mario.kainzbauer@gmail.com>
2024-01-20 13:44:42 +0100

    angular created

---
+++ API/Controllers/MarioController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API;
+
+[ApiController]
+[Route("[controller]")]
+public class MarioController : ControllerBase // route = domain.tld/Mario ("Mario"Controller without Controller is route)
+{
+    [HttpGet] // /mario
+    public IActionResult Mariov()
+    {
+        return Ok("Hello World");
+    }
+    [HttpGet("test")] // /mario/test
+    public IActionResult Mariox()
+    {
+        return Ok("Hello Worldx");
+    }
+}
