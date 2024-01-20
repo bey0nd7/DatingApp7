d66444364253f9378f9f439903c6a3addfb66e23
Mario Kainzbauer <mario.kainzbauer@gmail.com>
2024-01-20 16:20:22 +0100

    https angular .net

--- API/Program.cs
+++ API/Program.cs
@@ -18,7 +18,7 @@ WebApplication? app = builder.Build();
 // app.UseHttpsRedirection();
 // app.UseAuthorization();
 
-app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.MapControllers();
 
